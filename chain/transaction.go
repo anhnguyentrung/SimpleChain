c922ab16b9654a972cb35ef6367be8c8ce6bade3
anhnguyentrung <nguyentrunganh.bk@gmail.com>
2018-07-19 13:00:01 +0900

    pack and unpack transaction

--- chain/transaction.go
+++ chain/transaction.go
@@ -3,6 +3,10 @@ package chain
 import (
 	"time"
 	"blockchain/crypto"
+	bytes2 "bytes"
+	"compress/zlib"
+	"log"
+	"io/ioutil"
 )
 
 type TransactionHeader struct{
@@ -31,7 +35,8 @@ type PackedTransaction struct {
 	Signatures []crypto.Signature
 	Compression CompressionType
 	PackedContextFreeData []byte
-	PackedTransaction []byte
+	PackedTrx []byte
+	unPackedTrx *Transaction
 }
 
 // when the sender sends a transaction, they will assign it an id which will be passed back to sender if the transaction fails for some reason
@@ -40,6 +45,118 @@ type DeferredTransaction struct {
 	Sender AccountName
 	Payer AccountName
 	ExecuteAfter time.Time
+}
+
+func NewSignedTransaction(tx *Transaction) *SignedTransaction {
+	return &SignedTransaction{
+		*tx,
+		make([]crypto.Signature, 0),
+		make([]bytes, 0),
+	}
+}
+
+func (tx *Transaction) Pack(compression CompressionType) *PackedTransaction{
+	var packedTrx []byte
+	switch compression {
+	case None:
+		packedTrx, _ = MarshalBinary(*tx)
+	case Zlib:
+		packedTrx = tx.zlibCompress()
+	}
+	return &PackedTransaction{
+		Signatures: make([]crypto.Signature, 0),
+		Compression:compression,
+		PackedContextFreeData:make([]byte, 0),
+		PackedTrx: packedTrx,
+	}
+}
+
+func (s *SignedTransaction) Pack(compression CompressionType) *PackedTransaction {
+	tx := s.Transaction
+	var packedTrx []byte
+	packedContextFreeData, _ := MarshalBinary(s.ContextFreeData)
+	switch compression {
+	case None:
+		packedTrx, _ = MarshalBinary(tx)
+	case Zlib:
+		packedTrx = tx.zlibCompress()
+		packedContextFreeData = zlibCompress(packedContextFreeData)
+	}
+	return &PackedTransaction{
+		Signatures:s.Signatures,
+		Compression:compression,
+		PackedContextFreeData:packedContextFreeData,
+		PackedTrx:packedTrx,
+	}
+}
+
+func (p *PackedTransaction) unPack() {
+	out := zlibDecompress(p.PackedTrx)
+	decoder := NewDecoder(out)
+	var tx Transaction
+	err := decoder.Decode(&tx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.unPackedTrx = &tx
+}
+
+func (p *PackedTransaction) GetTransaction() *Transaction {
+	p.unPack()
+	return p.unPackedTrx
+}
+
+func (p *PackedTransaction) GetSingedTransaction() *SignedTransaction {
+	tx := p.GetTransaction()
+	s := NewSignedTransaction(tx)
+	s.Signatures = p.Signatures
+	switch p.Compression {
+	case None:
+		decoder := NewDecoder(p.PackedContextFreeData)
+		err := decoder.Decode(&s.ContextFreeData)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case Zlib:
+		decompressed := zlibDecompress(p.PackedContextFreeData)
+		decoder := NewDecoder(decompressed)
+		err := decoder.Decode(&s.ContextFreeData)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return s
+}
+
+func (tx *Transaction) zlibCompress() bytes {
+	in, _ := MarshalBinary(*tx)
+	var buffer bytes2.Buffer
+	w := zlib.NewWriter(&buffer)
+	w.Write(in)
+	w.Close()
+	return buffer.Bytes()
+}
+
+func zlibCompress(data bytes) bytes {
+	var buffer bytes2.Buffer
+	w := zlib.NewWriter(&buffer)
+	w.Write(data)
+	w.Close()
+	return buffer.Bytes()
+}
+
+func zlibDecompress(packedTrx []byte) bytes {
+	buffer := bytes2.NewReader(packedTrx)
+	r, err := zlib.NewReader(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+	decompressed, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return decompressed
 }
 
 
