af19f93c3057feac6736ef9c40ce7c97494a91cb
anhnguyentrung <nguyentrunganh.bk@gmail.com>
2018-08-01 22:19:41 +0900

    verify message signature by wasm

--- contract/execute.go
+++ contract/execute.go
@@ -3,16 +3,25 @@ package main
 import (
 	"os/exec"
 	"fmt"
+	"blockchain/crypto"
+	"crypto/sha256"
 	"os"
 )
 
 func main() {
 	args := os.Args[1:]
 	cmdArgs := []string{"wasm_exec.js"}
+	message := "hello"
+	priv, _ := crypto.NewRandomPrivateKey()
+	pubStr := priv.PublicKey().String()
+	hash := sha256.Sum256([]byte(message))
+	sign, _ := priv.Sign(hash[:])
+	signStr := sign.String()
+	args = append(args, pubStr, signStr, message)
 	cmdArgs = append(cmdArgs, args...)
 	out, err := exec.Command("node", cmdArgs...).Output()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("結果: %s", out)
+	fmt.Printf("結果: %s\n", out)
 }
--- contract/validatesign.go
+++ contract/validatesign.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"os"
+	"blockchain/crypto"
+	"crypto/sha256"
+	"fmt"
+)
+
 func main() {
-	//pubString := os.Args[1]
-	//signString := os.Args[2]
-	//sign := &crypto.Signature{}
-	//sign.
+	pubString := os.Args[1]
+	signString := os.Args[2]
+	message := os.Args[3]
+	hash := sha256.Sum256([]byte(message))
+	sign, _ := crypto.NewSignature(signString)
+	pub, _ := crypto.NewPublicKey(pubString)
+	fmt.Println(sign.Verify(hash[:], pub))
 }
