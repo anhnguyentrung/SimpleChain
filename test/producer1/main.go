61ec5c0bb4cf6412a949fb6e5facf187380b7b86
anhnguyentrung <nguyentrunganh.bk@gmail.com>
2018-08-14 20:42:42 +0900

    synchronise blockchain between nodes

--- test/producer1/main.go
+++ test/producer1/main.go
@@ -6,6 +6,7 @@ import (
 	"blockchain/wallet"
 	"fmt"
 	"log"
+	"blockchain/chain"
 )
 
 func createProducer1() (*wallet.SoftWallet, crypto.PublicKey) {
@@ -24,7 +25,9 @@ func createProducer1() (*wallet.SoftWallet, crypto.PublicKey) {
 	}
 	sw.UnLock("pass")
 	pub := sw.ListPublicKeys()[0]
+	priv := sw.ListKeys()[pub.String()]
 	fmt.Println("pub ", pub.String())
+	fmt.Println("priv ", priv.String())
 	return sw, pub
 }
 
@@ -35,12 +38,25 @@ func main() {
 	if privateKey.PublicKey().String() != pub.String() {
 		log.Fatal("key is wrong")
 	}
-	node := network.NewNode("0.0.0.0:2000", []string{"localhost:2001"})
+	node := network.NewNode("0.0.0.0:2000", []string{""})
+	producers := []chain.ProducerKey{}
+	for index, producerName := range chain.PRODUCER_NAMES {
+		producerPub, _ := crypto.NewPublicKey(chain.PRODUCER_PUBLIC_KEYS[index])
+		producerKey := chain.ProducerKey{
+			producerName,
+			producerPub,
+		}
+		producers = append(producers, producerKey)
+		node.AllowPeers = append(node.AllowPeers, producerPub)
+	}
 	node.NetworkVersion = 1
 	node.ChainId = node.BlockChain.ChainId
 	node.NodeId = node.ChainId
 	node.PrivateKeys[pub.String()] = privateKey
+	node.Producer.Producers = append(node.Producer.Producers, chain.AccountName("producer1"))
+	node.Producer.SignatureProviders[pub.String()] = network.MakeKeySignatureProvider(privateKey.String())
 	done := make(chan bool)
 	node.Start(true)
+	node.BlockChain.SetProposedProducers(producers)
 	<- done
 }
